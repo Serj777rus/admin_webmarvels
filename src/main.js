0dcd47a477eb685dc46f4fb908267b980aa9caf4
Sergey <gvsergey89@gmail.com>
2024-11-09 13:57:44 +0300

    с

--- src/main.js
+++ src/main.js
@@ -1,4 +1,24 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import AdminPanel from "@/components/AdminPanel.vue";
+import QuizPanel from "@/components/QuizPanel.vue";
 
-createApp(App).mount('#app')
+const router = createRouter({
+    routes: [
+        {
+            path: '/',
+            component: AdminPanel,
+            name: 'AdminPanel',
+        },
+        {
+            path: '/quiz',
+            component: QuizPanel,
+            name: 'QuizPanel',
+        }
+    ],
+    history: createWebHistory()
+})
+const app = createApp(App)
+app.use(router)
+app.mount('#app')
